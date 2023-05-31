ffe56e50e29e82aed22b8cc5f4dad10ab2e8f2e5
joshuauzor <joshuauzor10@gmail.com>
2023-05-31 09:11:51 +0100

    flutter clean

--- lib/core/dependency_injection/injection.config.dart
+++ lib/core/dependency_injection/injection.config.dart
@@ -6,13 +6,12 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i3;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:internet_connection_checker/internet_connection_checker.dart'
-    as _i5;
+    as _i4;
 
-import 'register_module.dart' as _i6; // ignore_for_file: unnecessary_lambdas
+import 'register_module.dart' as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -28,11 +27,9 @@ _i1.GetIt $initGetIt(
   );
   final registerModule = _$RegisterModule();
   gh.factory<_i3.Dio>(() => registerModule.dio);
-  gh.factory<_i4.FlutterSecureStorage>(
-      () => registerModule.flutterSecureStorage);
-  gh.factory<_i5.InternetConnectionChecker>(
+  gh.factory<_i4.InternetConnectionChecker>(
       () => registerModule.internetConnectionChecker);
   return get;
 }
 
-class _$RegisterModule extends _i6.RegisterModule {}
+class _$RegisterModule extends _i5.RegisterModule {}
--- lib/core/dependency_injection/register_module.dart
+++ lib/core/dependency_injection/register_module.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 
@@ -7,7 +6,6 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 abstract class RegisterModule {
   Dio get dio => Dio();
 
-  FlutterSecureStorage get flutterSecureStorage => const FlutterSecureStorage();
   InternetConnectionChecker get internetConnectionChecker =>
       InternetConnectionChecker();
 }
